df7d94f506537dde480ef414f8600a4047525d1f
Haris Mujovic <harismujovic@hotmail.com>
2021-04-06 18:58:47 +0200

    Added 'like' feature for users, as well as listing of users that liked the currently logged in user

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -24,20 +24,20 @@ export class MembersService {
       this.user = user;
       this.userParams = new UserParams(user);
     })
-   }
+  }
 
-   getUserParams(){
-     return this.userParams;
-   }
+  getUserParams() {
+    return this.userParams;
+  }
 
-   setUserParams(params: UserParams){
-     this.userParams = params;
-   }
+  setUserParams(params: UserParams) {
+    this.userParams = params;
+  }
 
-   resetUserParams() {
-     this.userParams = new UserParams(this.user);
-     return this.userParams;
-   }
+  resetUserParams() {
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+  }
 
   getMembers(userParams: UserParams) {
     var response = this.memberCache.get(Object.values(userParams).join('-'));
@@ -108,5 +108,15 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string){
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize){
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
   }
 }
